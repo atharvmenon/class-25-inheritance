f4b673d9102041529c483603761c083d5a255d30
atharvmenon <72930077+atharvmenon@users.noreply.github.com>
2020-11-19 19:51:23 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,13 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height) {
+   super(x,y,width,height);
+   this.image=loadImage("sprites/wood1.png");
+
+  }
+  
+};
+
+
+//parent class / base-class
+//child class/ sub-class
+//child/sub class inherits proerties and fucntions from the base/parent class
